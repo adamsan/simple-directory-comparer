0c6990bf0145bf6143ac2f6ee3b1b5839b1768fc
Adam Szentirmai-Schon <adamsan3@gmail.com>
2016-08-17 16:26:33 +0200

    move main to different class

---
+++ src/main/java/hu/adamsan/utilities/filecomparer/App.java
@@ -0,0 +1,60 @@
+package hu.adamsan.utilities.filecomparer;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+
+public class App {
+
+    private DirectoryComparator directoryComparator;
+    private SearchPersistence searchPersistence;
+
+    public DirectoryComparator getDirectoryComparator() {
+        return directoryComparator;
+    }
+
+    public void setDirectoryComparator(DirectoryComparator directoryComparator) {
+        this.directoryComparator = directoryComparator;
+    }
+
+    public void setPersistence(SearchPersistence searchPersistence) {
+        this.searchPersistence = searchPersistence;
+    }
+
+    public void start() {
+        Pair<File> previousSelection = searchPersistence.loadPreviousSelection();
+        Pair<File> dirs = select(previousSelection);
+        if (!dirs.isNull()) {
+            searchPersistence.saveSelection(dirs);
+            directoryComparator.setDirectories(dirs.first.toPath(), dirs.second.toPath());
+            List<Path> difference = directoryComparator.calculateDifference();
+            displayDifference(difference);
+        }
+    }
+
+    private Pair<File> select(Pair<File> previousSelection) {
+        Pair<File> pair = new Pair<>();
+        pair.first = selectDirectory(previousSelection.first);
+        if (pair.first != null) {
+            pair.second = selectDirectory(previousSelection.second);
+        }
+        return pair;
+    }
+
+    private File selectDirectory(File previousFile) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        if (previousFile != null) {
+            chooser.setSelectedFile(previousFile);
+        }
+        chooser.showOpenDialog(null);
+        return chooser.getSelectedFile();
+    }
+
+    private void displayDifference(List<Path> difference) {
+        System.out.println(difference);
+    }
+
+}
--- src/main/java/hu/adamsan/utilities/filecomparer/Main.java
+++ src/main/java/hu/adamsan/utilities/filecomparer/Main.java
@@ -1,67 +1,11 @@
 package hu.adamsan.utilities.filecomparer;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.List;
-
-import javax.swing.JFileChooser;
-
 public class Main {
 
-    private DirectoryComparator directoryComparator;
-    private SearchPersistence searchPersistence;
-
-    public DirectoryComparator getDirectoryComparator() {
-        return directoryComparator;
-    }
-
-    public void setDirectoryComparator(DirectoryComparator directoryComparator) {
-        this.directoryComparator = directoryComparator;
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        main.setDirectoryComparator(new DirectoryComparator());
-        main.setPersistence(new SearchPersistence());
-        main.start();
+        App program = new App();
+        program.setDirectoryComparator(new DirectoryComparator());
+        program.setPersistence(new SearchPersistence());
+        program.start();
     }
-
-    private void setPersistence(SearchPersistence searchPersistence) {
-        this.searchPersistence = searchPersistence;
-    }
-
-    private void start() {
-        Pair<File> previousSelection = searchPersistence.loadPreviousSelection();
-        Pair<File> dirs = select(previousSelection);
-        if (!dirs.isNull()) {
-            searchPersistence.saveSelection(dirs);
-            directoryComparator.setDirectories(dirs.first.toPath(), dirs.second.toPath());
-            List<Path> difference = directoryComparator.calculateDifference();
-            displayDifference(difference);
-        }
-    }
-
-    private Pair<File> select(Pair<File> previousSelection) {
-        Pair<File> pair = new Pair<>();
-        pair.first = selectDirectory(previousSelection.first);
-        if (pair.first != null) {
-            pair.second = selectDirectory(previousSelection.second);
-        }
-        return pair;
-    }
-
-    private File selectDirectory(File previousFile) {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-        if (previousFile != null) {
-            chooser.setSelectedFile(previousFile);
-        }
-        chooser.showOpenDialog(null);
-        return chooser.getSelectedFile();
-    }
-
-    private void displayDifference(List<Path> difference) {
-        System.out.println(difference);
-    }
-
 }
